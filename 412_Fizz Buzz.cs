d3e769fd7c7a86221ee1d60e13e93dc25029d8f7
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-10-31 15:42:19 +0800

    412_Fizz Buzz

---
+++ 412_Fizz Buzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        List<string> list = new List<string>();
+        for (int i = 1; i <= n; i++)
+        {
+            // Convert int to string
+            string strNumber = i.ToString();
+
+            // Check for 3
+            bool hasFactor3 = false;
+            int sum = 0;
+            for (int j = 0; j < strNumber.Length; j++)
+                sum += Int32.Parse(strNumber[j].ToString());
+            if (sum % 3 == 0)
+                hasFactor3 = true;
+
+            // Check for 5
+            bool hasFactor5 = false;
+            int lastDigit = Int32.Parse(strNumber[strNumber.Length - 1].ToString());
+            if (lastDigit == 0 || lastDigit == 5)
+                hasFactor5 = true;
+
+            // Make the string and push it to list
+            string str = "";
+            if (hasFactor3)
+                str += "Fizz";
+            if (hasFactor5)
+                str += "Buzz";
+            if (!hasFactor3 && !hasFactor5)
+                str = i.ToString();
+            list.Add(str);
+        }
+
+        return list;
+    }
+}
