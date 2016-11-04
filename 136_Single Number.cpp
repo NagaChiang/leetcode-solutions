476ce8eae805379f5b29dde399b7a3de777d2ba3
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-04 15:46:31 +0800

    136_Single Number

---
+++ 136_Single Number.cpp
@@ -0,0 +1,18 @@
+﻿class Solution
+{
+
+public:
+
+	int singleNumber(vector<int>& nums)
+	{
+		// The sum of XOR every element = the single one
+		int sumXor = 0;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			sumXor ^= nums[i];
+		}
+
+		return sumXor;
+	}
+
+};
