893ba7669b80a3b3a49be1111320b718751f771c
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-04 16:31:57 +0800

    371_Sum of Two Integers

---
+++ 371_Sum of Two Integers.cpp
@@ -0,0 +1,33 @@
+﻿class Solution
+{
+
+public:
+
+	int getSum(int a, int b)
+	{
+		// One of them is 0
+		if(a == 0)
+			return b;
+		if(b == 0)
+			return a;
+
+		// Calculate
+		int aClone = a;
+		int bClone = b;
+		int carry = 0;
+		while(bClone != 0)
+		{
+			// Carry
+			carry = aClone & bClone;
+
+			// Add (without carry)
+			aClone ^= bClone;
+
+			// Move carry and assign it to b
+			bClone = carry << 1;
+		}
+
+		return aClone;
+	}
+
+};
