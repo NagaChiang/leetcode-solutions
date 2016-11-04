b6d40e8638ca8061849b3047e76061ef945aa791
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-04 15:36:21 +0800

    344_Reverse String

---
+++ 344_Reverse String.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+
+class Solution
+{
+
+public:
+
+	std::string reverseString(std::string s)
+    {
+		std::string clone = std::string(s);
+        char* ptrFront = &clone[0];
+        char* ptrEnd = &clone[clone.size() - 1];
+        while(ptrEnd >= ptrFront)
+        {
+            // Reach the same index
+            if (ptrFront == ptrEnd)
+                break;
+
+            // Swap
+            swapChar(ptrFront, ptrEnd);
+
+            // Move pointers
+            ptrFront++;
+            ptrEnd--;
+        }
+
+		return clone;
+    }
+
+private:
+
+    void swapChar(char* a, char* b)
+    {
+        char temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+};
