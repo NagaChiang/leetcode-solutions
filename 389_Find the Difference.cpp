e72d71daa358308d3b07bf92711d83952d75844d
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-05 18:23:57 +0800

    389_Find the Difference

---
+++ 389_Find the Difference.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+
+class Solution
+{
+
+public:
+
+	char findTheDifference(std::string s, std::string t)
+	{
+		// Sum of XOR = the different one char
+		char sum = 0;
+		for(int i = 0; i < s.length(); i++)
+			sum ^= s[i];
+		for(int i = 0; i < t.length(); i++)
+			sum ^= t[i];
+
+		return sum;
+	}
+
+};
