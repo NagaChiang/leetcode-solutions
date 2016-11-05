046a8745e58cbc86af8018242a226daff41d2432
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-05 21:59:26 +0800

    283_Move Zeroes

---
+++ 283_Move Zeroes.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+
+
+
+// 1. All elements before the slow pointer (lastZeroIndex) are non-zeroes.
+// 2. All elements between the current and slow pointer are zeroes.
+class Solution {
+
+public:
+
+	void moveZeroes(std::vector<int>& nums)
+	{
+		int lastZeroIndex = 0;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			if(nums[i] != 0)
+			{
+				std::swap(nums[lastZeroIndex], nums[i]);
+				lastZeroIndex++;
+			}
+		}
+	}
+
+};
