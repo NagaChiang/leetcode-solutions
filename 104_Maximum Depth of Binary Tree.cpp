7b7b9000488be924cdc742ae72d4355fce9ebb80
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-05 16:13:47 +0800

    104_Maximum Depth of Binary Tree

---
+++ 104_Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,71 @@
+﻿#include <queue>
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// Recursive DFS
+// 6 ms
+class Solution
+{
+
+public:
+
+	int maxDepth(TreeNode* root)
+	{
+		// Check null
+		if(root == NULL)
+			return 0;
+
+		// Traverse left and right tree
+		int depthLeft = maxDepth(root->left);
+		int depthRight = maxDepth(root->right);
+
+		// Return the larger one
+		return max(depthLeft, depthRight) + 1;
+	}
+};
+
+// Iterative BFS
+// 9 ms
+class Solution
+{
+
+public:
+
+	int maxDepth(TreeNode* root)
+	{
+		// Check null
+		if(root == NULL)
+			return 0;
+
+		// BFS
+		int depth = 0;
+		int numLevelNodes = 0;
+		std::queue<TreeNode*> queueNodes;
+		queueNodes.push(root);
+		while(!queueNodes.empty())
+		{
+			// Clear a level
+			if(numLevelNodes == 0)
+			{
+				depth++;
+				numLevelNodes = queueNodes.size();
+			}
+
+			TreeNode* node = queueNodes.front();
+			queueNodes.pop();
+			numLevelNodes--;
+			if(node->left)
+				queueNodes.push(node->left);
+			if(node->right)
+				queueNodes.push(node->right);
+		}
+
+		return depth;
+	}
+};
