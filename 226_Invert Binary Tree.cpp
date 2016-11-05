4e7bd0547134358590f4cfa0645d6eeb8340aba0
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-05 18:40:19 +0800

    226_Invert Binary Tree

---
+++ 226_Invert Binary Tree.cpp
@@ -0,0 +1,34 @@
+﻿struct TreeNode
+{
+	int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+// Recursive DFS
+class Solution
+{
+
+public:
+
+	TreeNode* invertTree(TreeNode* root)
+	{
+		// Check null
+		if(root == NULL)
+			return NULL;
+
+		// Swap left and right
+		TreeNode* temp = root->left;
+		root->left = root->right;
+		root->right = temp;
+
+		// Invert subtrees
+		invertTree(root->left);
+		invertTree(root->right);
+
+		return root;
+	}
+
+};
