e37971f7ccd8f016aaa38932bbf66d71652b077e
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-06 16:07:20 +0800

    349. Intersection of Two Arrays

---
+++ 349_Intersection of Two Arrays.cpp
@@ -0,0 +1,56 @@
+﻿#include <vector>
+
+// Note: It's tricky to use big array instead of map.
+
+class Solution
+{
+
+public:
+
+	std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2)
+	{
+		// Record element in the shorter vector
+		std::vector<int> shortNums, longNums;
+		if(nums1.size() >= nums2.size())
+		{
+			// nums2 is shorter
+			shortNums = nums2;
+			longNums = nums1;
+		}
+		else
+		{
+			// nums1 is shorter
+			shortNums = nums1;
+			longNums = nums2;
+		}
+
+		// Record each element contained in nums1
+		const int SIZE = 1000;
+		bool isContained[SIZE] = { false };
+		for(int i = 0; i < shortNums.size(); i++)
+		{
+			isContained[shortNums[i]] = true;
+		}
+
+		// Compared to nums2
+		bool isBothContained[SIZE] = { false };
+		for(int i = 0; i < longNums.size(); i++)
+		{
+			int num = longNums[i];
+			if(isContained[num])
+			{
+				isBothContained[num] = true;
+			}
+		}
+
+		// Convert map to vector
+		std::vector<int> intersection;
+		for(int i = 0; i < SIZE; i++)
+		{
+			if(isBothContained[i])
+				intersection.push_back(i);
+		}
+
+		return intersection;
+	}
+};
