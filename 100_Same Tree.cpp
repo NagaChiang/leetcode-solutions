fe81870b9d5dd822ad6a929d1d9e4916cb380644
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-06 20:15:38 +0800

    100. Same Tree

---
+++ 100_Same Tree.cpp
@@ -0,0 +1,83 @@
+﻿#include <queue>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// BFS
+// 3 ms
+class Solution
+{
+
+public:
+
+	bool isSameTree(TreeNode* p, TreeNode* q)
+	{
+		std::queue<TreeNode*> queueP;
+		std::queue<TreeNode*> queueQ;
+		queueP.push(p);
+		queueQ.push(q);
+		TreeNode* nodeP;
+		TreeNode* nodeQ;
+		while(!queueP.empty() && !queueQ.empty())
+		{
+			nodeP = queueP.front();
+			nodeQ = queueQ.front();
+			queueP.pop();
+			queueQ.pop();
+
+			// Both null
+			if(!nodeP && !nodeQ)
+				continue;
+
+			// One null
+			if((!nodeP && nodeQ) || (nodeP && !nodeQ))
+				return false;
+
+			// Not the same value
+			if(nodeP->val != nodeQ->val)
+				return false;
+
+			// Push nodes to the queue
+			queueP.push(nodeP->left);
+			queueQ.push(nodeQ->left);
+			queueP.push(nodeP->right);
+			queueQ.push(nodeQ->right);
+		}
+
+		return true;
+	}
+};
+
+// Recursive
+// 0 ms
+class Solution
+{
+
+public:
+
+	bool isSameTree(TreeNode* p, TreeNode* q)
+	{
+		// Both null
+		if(!p && !q)
+			return true;
+
+		// One null
+		if((!p && q) || (p && !q))
+			return false;
+
+		// Both have value
+		if(p->val != q->val)
+			return false;
+		else
+		{
+			// Check sub trees
+			return isSameTree(p->left, q->left)
+				&& isSameTree(p->right, q->right);
+		}
+	}
+};
