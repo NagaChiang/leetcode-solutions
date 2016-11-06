8d795740a7cadc44af830a3f4c89a55d564e0b10
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-06 16:28:27 +0800

    237. Delete Node in a Linked List

---
+++ 237_Delete Node in a Linked List.cpp
@@ -0,0 +1,32 @@
+﻿#include <string.h>
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+
+public:
+
+	void deleteNode(ListNode* node)
+	{
+		node->val = node->next->val;
+		node->next = node->next->next;
+	}
+};
+
+// Memory copy
+class Solution
+{
+
+public:
+
+	void deleteNode(ListNode* node)
+	{
+		memcpy(node, node->next, sizeof(ListNode));
+	}
+};
