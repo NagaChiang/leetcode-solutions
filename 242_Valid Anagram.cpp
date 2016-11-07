6097dc00ae4b7ea3101205c635b2d3f706cd4d97
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-07 15:17:59 +0800

    242. Valid Anagram

---
+++ 242_Valid Anagram.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <string.h>
+
+class Solution
+{
+
+public:
+
+	bool isAnagram(std::string s, std::string t)
+	{
+		// Different length
+		if(s.length() != t.length())
+			return false;
+
+		// Count characters
+		const int OFFSET = 'a';
+		const int SIZE = 26;
+		int charCounts_1[SIZE] = { 0 };
+		int charCounts_2[SIZE] = { 0 };
+		for(int i = 0; i < s.length(); i++)
+		{
+			charCounts_1[s[i] - OFFSET]++;
+			charCounts_2[t[i] - OFFSET]++;
+		}
+
+		// Compare
+		if(memcmp(charCounts_1, charCounts_2, sizeof(int) * SIZE) == 0)
+			return true;
+		else
+			return false;
+	}
+};
