2a9997c12f1f523710021847492b090aced83434
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-07 14:48:50 +0800

    387. First Unique Character in a String

---
+++ 387_First Unique Character in a String.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+
+class Solution
+{
+
+public:
+
+	int firstUniqChar(std::string s)
+	{
+		// Count characters
+		const int OFFSET = 'a';
+		int charCounts[26] = { 0 };
+		for(int i = 0; i < s.length(); i++)
+		{
+			charCounts[s[i] - OFFSET]++;
+		}
+		
+		// Find the first unique index
+		int indexUnique = -1;
+		for(int i = 0; i < s.length(); i++)
+		{
+			if(charCounts[s[i] - OFFSET] < 2)
+			{
+				indexUnique = i;
+				break;
+			}
+		}
+
+		return indexUnique;
+	}
+};
