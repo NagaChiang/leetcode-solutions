7fd929823a62dcf873af2f840c3e70f4efe5d5c9
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-07 11:54:02 +0800

    171. Excel Sheet Column Number

---
+++ 171_Excel Sheet Column Number.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <math.h>
+
+class Solution
+{
+
+public:
+
+	int titleToNumber(std::string s)
+	{
+		const int NUM_ALPHABET = 26;
+		int num = 0;
+		for(int i = 0; i < s.length(); i++)
+		{
+			int index = s.length() - i - 1;
+			num += alphabetToNumber(s[index]) * pow(NUM_ALPHABET, i);
+		}
+
+		return num;
+	}
+
+private:
+
+	int alphabetToNumber(char c)
+	{
+		return c - 'A' + 1;
+	}
+};
