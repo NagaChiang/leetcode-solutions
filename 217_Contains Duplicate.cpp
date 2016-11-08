4c8f373a71a6e8fb8b78543d328dedb931525060
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-08 15:14:18 +0800

    217. Contains Duplicate

---
+++ 217_Contains Duplicate.cpp
@@ -0,0 +1,55 @@
+﻿#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+
+public:
+
+	bool containsDuplicate(std::vector<int>& nums)
+	{
+		// Count elements
+		bool hasDuplicate = false;
+		std::unordered_map<int, int> numberCounts;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			numberCounts[nums[i]]++;
+
+			// Check duplicate
+			if(numberCounts[nums[i]] > 1)
+			{
+				hasDuplicate = true;
+				break;
+			}
+		}
+
+		return hasDuplicate;
+	}
+};
+
+// Array version
+class Solution
+{
+
+public:
+
+	bool containsDuplicate(std::vector<int>& nums)
+	{
+		// Count elements
+		bool hasDuplicate = false;
+		int numberCounts[1000000] = { 0 };
+		for(int i = 0; i < nums.size(); i++)
+		{
+			numberCounts[nums[i]]++;
+
+			// Check duplicate
+			if(numberCounts[nums[i]] > 1)
+			{
+				hasDuplicate = true;
+				break;
+			}
+		}
+
+		return hasDuplicate;
+	}
+};
