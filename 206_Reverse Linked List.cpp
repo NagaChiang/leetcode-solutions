4c0ab0e9f7bf67c9d97e15a2ccb1f967a62d9527
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-09 18:38:48 +0800

    206. Reverse Linked List

---
+++ 206_Reverse Linked List.cpp
@@ -0,0 +1,52 @@
+﻿#include <string.h>
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Iterative
+// 6 ms
+class Solution
+{
+
+public:
+
+	ListNode* reverseList(ListNode* head)
+	{
+		ListNode* lastNode = NULL;
+		ListNode* node = head;
+		while(node != NULL)
+		{
+			// Reverse
+			ListNode* nextNode = node->next;
+			node->next = lastNode;
+
+			// Move pointers
+			lastNode = node;
+			node = nextNode;
+		}
+
+		return lastNode;
+	}
+};
+
+// Recursive
+// 6 ms
+class Solution
+{
+
+public:
+
+	ListNode* reverseList(ListNode* head)
+	{
+		if(head == NULL || head->next == NULL)
+			return head;
+
+		ListNode* reversed = reverseList(head->next);
+		head->next->next = head;
+		head->next = NULL;
+		return reversed;
+	}
+};
