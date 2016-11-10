c9f763126427eefbcdacd150f5ddd67460703c79
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-10 18:18:43 +0800

    453. Minimum Moves to Equal Array Elements

---
+++ 453_Minimum Moves to Equal Array Elements.cpp
@@ -0,0 +1,29 @@
+﻿#include <limits>
+#include <vector>
+
+// Note: Add n-1 elements by 1 = Substract 1 element by 1
+
+// 73 ms
+class Solution
+{
+
+public:
+
+	int minMoves(std::vector<int>& nums)
+	{
+		// Find min
+		int min = std::numeric_limits<int>::max();
+		for(int i = 0; i < nums.size(); i++)
+		{
+			if(nums[i] < min)
+				min = nums[i];
+		}
+
+		// Sum of the difference between min and other elements
+		int sum = 0;
+		for(int i = 0; i < nums.size(); i++)
+			sum += nums[i] - min;
+
+		return sum;
+	}
+};
