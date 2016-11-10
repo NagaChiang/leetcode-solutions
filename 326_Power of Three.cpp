73f86636717693e93bb370101dc69c3eb4a50099
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-10 15:35:09 +0800

    326. Power of Three

---
+++ 326_Power of Three.cpp
@@ -0,0 +1,36 @@
+﻿// Keep dividing
+// 48 ms
+class Solution
+{
+
+public:
+
+	bool isPowerOfThree(int n)
+	{
+		// Zero or negtive
+		if(n <= 0)
+			return false;
+
+		// Divide by 3
+		while(n % 3 == 0)
+		{
+			n /= 3;
+		}
+
+		// Check if the remain is 1
+		return n == 1;
+	}
+};
+
+// Integer limitations
+// 65 ms
+class Solution
+{
+
+public:
+
+	bool isPowerOfThree(int n)
+	{
+		return n > 0 && 1162261467 % n == 0;
+	}
+};
