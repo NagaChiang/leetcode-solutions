31bf9bd780979e79aed8d6c95bef5b903c98289a
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-10 15:38:08 +0800

    231. Power of Two

---
+++ 231_Power of Two.cpp
@@ -0,0 +1,21 @@
+﻿class Solution
+{
+
+public:
+
+	bool isPowerOfTwo(int n)
+	{
+		// Zero or negtive
+		if(n <= 0)
+			return false;
+
+		// Divide by 2
+		while(n % 2 == 0)
+		{
+			n /= 2;
+		}
+
+		// Check if the remain is 1
+		return n == 1;
+	}
+};
