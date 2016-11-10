ee7064a26c8781051dac501eb60cf441e65f069d
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-10 16:40:51 +0800

    447. Number of Boomerangs

---
+++ 447_Number of Boomerangs.cpp
@@ -0,0 +1,35 @@
+﻿#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+
+public:
+
+	int numberOfBoomerangs(std::vector<std::pair<int, int>>& points)
+	{
+		int sum = 0;
+
+		// For every point as center
+		for(int i = 0; i < points.size(); i++)
+		{
+			// Distance to other points	
+			std::unordered_map<int, int> mapDistance;
+			for(int j = 0; j < points.size(); j++)
+			{
+				int dx = points[i].first - points[j].first;
+				int dy = points[i].second - points[j].second;
+				mapDistance[dx*dx + dy*dy]++;
+			}
+
+			// Calculate combinations
+			for(auto itr = mapDistance.begin(); itr != mapDistance.end(); itr++)
+			{
+				int amount = itr->second;
+				sum += amount * (amount - 1);
+			}
+		}
+
+		return sum;
+	}
+};
