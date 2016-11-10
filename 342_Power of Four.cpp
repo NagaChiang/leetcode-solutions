7c70811ab627c2b72db366b82360a9d726692424
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-10 15:39:06 +0800

    342. Power of Four

---
+++ 342_Power of Four.cpp
@@ -0,0 +1,21 @@
+﻿class Solution
+{
+
+public:
+
+	bool isPowerOfFour(int n)
+	{
+		// Zero or negtive
+		if(n <= 0)
+			return false;
+
+		// Divide by 4
+		while(n % 4 == 0)
+		{
+			n /= 4;
+		}
+
+		// Check if the remain is 1
+		return n == 1;
+	}
+};
