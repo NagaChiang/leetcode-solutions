508850805bc088d7b2b1f25b54ea90567db91d6d
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-11 14:38:43 +0800

    121. Best Time to Buy and Sell Stock

---
+++ 121_Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,28 @@
+﻿#include <limits>
+#include <vector>
+
+// Greedy
+class Solution
+{
+
+public:
+
+	int maxProfit(std::vector<int>& prices)
+	{
+		int minPrice = std::numeric_limits<int>().max();
+		int maximumProfit = 0;
+		for(int i = 0; i < prices.size(); i++)
+		{
+			// Min price
+			if(prices[i] < minPrice)
+				minPrice = prices[i];
+
+			// Max profit
+			int profit = prices[i] - minPrice;
+			if(profit > maximumProfit)
+				maximumProfit = profit;
+		}
+
+		return maximumProfit;
+	}
+};
