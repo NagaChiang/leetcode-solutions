ab86010c72b71d71cc832655fd7f2c5cf9519243
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-11 15:42:43 +0800

    070. Climbing Stairs

---
+++ 070_Climbing Stairs.cpp
@@ -0,0 +1,25 @@
+﻿// Dynamic programming
+// 6 ms
+class Solution
+{
+
+public:
+
+	int climbStairs(int n)
+	{
+		// Small numbers
+		if(n <= 2)
+			return n > 0 ? n : 0;
+
+		// DP
+		int climbWays[n + 1] = { 0 };
+		climbWays[1] = 1;
+		climbWays[2] = 2;
+		for(int i = 3; i < n + 1; i++)
+		{
+			climbWays[i] = climbWays[i - 1] + climbWays[i - 2];
+		}
+
+		return climbWays[n];
+	}
+};
