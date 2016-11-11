c3527ac58a9f48722ce1c67a8f999a1994299da0
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-11 15:19:49 +0800

    083. Remove Duplicates from Sorted List

---
+++ 083_Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,34 @@
+﻿#include <string.h>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+
+public:
+
+	ListNode* deleteDuplicates(ListNode* head)
+	{
+		ListNode* node = head;
+		while(node && node->next)
+		{
+			if(node->val == node->next->val)
+			{
+				ListNode* nextNode = node->next;
+				node->next = node->next->next;
+				free(nextNode);
+			}
+			else
+			{
+				node = node->next;
+			}
+		}
+
+		return head;
+	}
+};
