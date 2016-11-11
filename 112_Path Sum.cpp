bc2841f5984f4c6e09c21cd6915f6b11b005437f
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-11 16:21:57 +0800

    112. Path Sum

---
+++ 112_Path Sum.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdlib.h>
+
+struct TreeNode
+{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+
+public:
+
+	bool hasPathSum(TreeNode* root, int sum)
+	{
+		if(root == NULL)
+			return false;
+		else if(root->left == NULL && root->right == NULL) // Leaf
+			return sum == root->val ? true : false;
+		else
+			return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+	}
+};
