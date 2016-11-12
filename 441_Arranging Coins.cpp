4ee6c09bc29d7d551bf44c359b7da8917efd01c6
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-12 20:35:10 +0800

    441. Arranging Coins

---
+++ 441_Arranging Coins.cpp
@@ -0,0 +1,35 @@
+﻿#include <math.h>
+
+class Solution
+{
+
+public:
+
+	int arrangeCoins(int n)
+	{
+		int numRow = 0;
+		for(int i = 1; n >= 0; i++)
+		{
+			// Row
+			n -= i;
+
+			// Check remaining
+			if(n >= 0)
+				numRow++;
+		}
+
+		return numRow;
+	}
+};
+
+// Math
+class Solution
+{
+
+public:
+
+	int arrangeCoins(int n)
+	{
+		return (int)(sqrt(8 * (long)n + 1) - 1) / 2;
+	}
+};
