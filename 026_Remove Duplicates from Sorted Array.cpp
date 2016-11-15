42bfa80ea49fee746c80e28ddf8ea1eac8b1bf7b
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-15 14:38:27 +0800

    26. Remove Duplicates from Sorted Array

---
+++ 026_Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+class Solution
+{
+
+public:
+
+	int removeDuplicates(std::vector<int>& nums)
+	{
+		if(nums.size() == 0)
+			return 0;
+
+		int scanned = 0;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			// Get the value of fast pointer
+			if(nums[i] != nums[scanned])
+			{
+				scanned++;
+				nums[scanned] = nums[i];
+			}
+		}
+
+		return scanned + 1;
+	}
+};
