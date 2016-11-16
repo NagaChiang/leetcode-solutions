b1fa290d8f1c1b14808a303389380f0eec0aaaae
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-16 21:02:45 +0800

    172. Factorial Trailing Zeroes

---
+++ 172_Factorial Trailing Zeroes.cpp
@@ -0,0 +1,20 @@
+﻿// Note: The number of 2s are much more than 5s.
+
+class Solution
+{
+
+public:
+
+	int trailingZeroes(int n)
+	{
+		// Count 5 factors
+		int numFive = 0;
+		while(n >= 5)
+		{
+			numFive += n / 5;
+			n /= 5;
+		}
+
+		return numFive;
+	}
+};
