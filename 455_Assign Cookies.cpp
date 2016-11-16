35773ce92390397a39abd7ebea9be4540a8e2db7
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-16 15:48:03 +0800

    455. Assign Cookies

---
+++ 455_Assign Cookies.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+#include <algorithm>
+
+class Solution
+{
+
+public:
+
+	int findContentChildren(std::vector<int>& g, std::vector<int>& s)
+	{
+		// Sorting
+		std::sort(g.begin(), g.end());
+		std::sort(s.begin(), s.end());
+
+		// Greedy
+		int num = 0;
+		for(int i = 0, j = 0;
+			i < g.size() && j < s.size();)
+		{
+			if(g[i] <= s[j])
+			{
+				// Content
+				num++;
+				i++;
+				j++;
+			}
+			else
+			{
+				// Bigger cookie
+				j++;
+			}
+		}
+
+		return num;
+	}
+};
