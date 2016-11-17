688ab3dcd9ee8fc48e7576a813a1d7305b296496
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-17 15:21:40 +0800

    9. Palindrome Number

---
+++ 009_Palindrome Number.cpp
@@ -0,0 +1,34 @@
+﻿class Solution
+{
+
+public:
+
+	bool isPalindrome(int x)
+	{
+		if(x < 0)
+			return false;
+
+		// Find the divide for high digits
+		int highDiv = 1;
+		while(x / highDiv >= 10)
+		{
+			highDiv *= 10;
+		}
+
+		// Check
+		int lowDiv = 1;
+		while(highDiv >= lowDiv)
+		{
+			int highDigit = x / highDiv % 10;
+			int lowDigit = x / lowDiv % 10;
+			if(highDigit != lowDigit)
+				return false;
+
+			// Next divides
+			highDiv /= 10;
+			lowDiv *= 10;
+		}
+
+		return true;
+	}
+};
