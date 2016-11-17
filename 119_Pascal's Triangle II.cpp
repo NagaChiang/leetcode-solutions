5414655760c34926f4565faecc1e255239cf4100
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-17 11:50:33 +0800

    119. Pascal's Triangle II

---
+++ 119_Pascal's Triangle II.cpp
@@ -0,0 +1,38 @@
+﻿#include <vector>
+
+// Math
+class Solution
+{
+
+public:
+
+	std::vector<int> getRow(int rowIndex)
+	{
+		if(rowIndex == 0)
+		{
+			std::vector<int> oneRow;
+			oneRow.push_back(1);
+			return oneRow;
+		}
+
+		// Using formula
+		int size = rowIndex + 1;
+		int center = size / 2;
+		std::vector<int> row;
+		row.push_back(1);
+		for(int i = 1; i <= center; i++)
+		{
+			// Construct half row
+			int num = (long)row[i - 1] * (rowIndex - i + 1) / i;
+			row.push_back(num);
+		}
+
+		// Add another half by mirroring
+		for(int i = size - (center + 1) - 1; i >= 0; i--)
+		{
+			row.push_back(row[i]);
+		}
+
+		return row;
+	}
+};
