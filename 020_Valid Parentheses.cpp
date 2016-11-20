a00615e2dec63ed0c3d10ec5ecfd0263e9858682
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-20 14:00:07 +0800

    20. Valid Parentheses

---
+++ 020_Valid Parentheses.cpp
@@ -0,0 +1,56 @@
+﻿#include <string>
+#include <stack>
+
+class Solution
+{
+
+public:
+
+	bool isValid(std::string s)
+	{
+		std::stack<char> stackChar;
+		for(int i = 0; i < s.length(); i++)
+		{
+			char c = s[i];
+			if(c == '(' || c == '[' || c == '{')
+			{
+				// Start
+				stackChar.push(c);
+			}
+			else if(c == ')' || c == ']' || c == '}')
+			{
+				// End
+				if(!stackChar.empty())
+				{
+					// Check
+					char top = stackChar.top();
+					if((top == '(' && c == ')')
+						|| (top == '[' && c == ']')
+						|| (top == '{' && c == '}'))
+					{
+						// Pop
+						stackChar.pop();
+					}
+					else
+					{
+						// Invalid
+						return false;
+					}
+				}
+				else
+				{
+					// Empty stack
+					return false;
+				}
+			}
+		}
+
+		// Stack is stiil not empty
+		if(!stackChar.empty())
+		{
+			return false;
+		}
+
+		return true;
+	}
+};
