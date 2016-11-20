d8978b6d395d8fa126c9254d82da36e68973c3f6
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-20 14:33:53 +0800

    219. Contains Duplicate II

---
+++ 219_Contains Duplicate II.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <unordered_map>
+
+class Solution
+{
+
+public:
+
+	bool containsNearbyDuplicate(std::vector<int>& nums, int k)
+	{
+		std::unordered_map<int, int> lastIndex;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			int element = nums[i];
+			if(lastIndex.find(element) != lastIndex.end())
+			{
+				// Has appeared
+				if(i - lastIndex[element] <= k)
+					return true;
+			}
+			
+			// Remember the index
+			lastIndex[element] = i;
+		}
+
+		return false;
+	}
+};
