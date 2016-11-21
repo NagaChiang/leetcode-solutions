5506b92c8ea5ef65442bb82dd7ecda7b90265d68
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-21 11:34:30 +0800

    203. Remove Linked List Elements

---
+++ 203_Remove Linked List Elements.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdlib.h>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+
+public:
+
+	ListNode* removeElements(ListNode* head, int val)
+	{
+		ListNode* prehead = new ListNode(0);
+		prehead->next = head;
+		ListNode* node = prehead;
+		while(node)
+		{
+			// Check next node
+			if(node->next && node->next->val == val)
+			{
+				// Remove next node
+				node->next = node->next->next;
+			}
+			else
+			{
+				// Move node
+				node = node->next;
+			}
+		}
+
+		return prehead->next;
+	}
+};
