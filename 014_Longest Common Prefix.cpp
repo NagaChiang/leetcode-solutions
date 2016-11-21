d77681a31b4384f39d28038ab8491cf37f8a940f
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-21 11:52:09 +0800

    14. Longest Common Prefix

---
+++ 014_Longest Common Prefix.cpp
@@ -0,0 +1,65 @@
+﻿#include <vector>
+#include <string>
+
+class Solution
+{
+
+public:
+
+	std::string longestCommonPrefix(std::vector<std::string>& strs)
+	{
+		// Edge cases
+		if(strs.size() == 0)
+			return "";
+		else if(strs.size() == 1)
+			return strs[0];
+
+		// Check from the start of strings
+		std::string commonPrefix = "";
+		for(int i = 0;; i++)
+		{
+			// Check each string
+			bool isSame = true;
+			char common = 0;
+			for(int j = 0; j < strs.size(); j++)
+			{
+				// Check length
+				if(i < strs[j].length())
+				{
+					// Set reference character
+					if(common <= 0)
+						common = strs[j][i];
+					else
+					{
+						// Check character
+						if(strs[j][i] != common)
+						{
+							// Different
+							isSame = false;
+							break;
+						}
+					}
+				}
+				else
+				{
+					// Not enough length
+					isSame = false;
+					break;
+				}
+			}
+
+			// Check same
+			if(isSame)
+			{
+				// Append character to common prefix
+				commonPrefix.push_back(common);
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return commonPrefix;
+	}
+};
