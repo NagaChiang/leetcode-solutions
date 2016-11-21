f3874621f17cd900f1b9985d044e8598762b8039
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-21 11:07:09 +0800

    58. Length of Last Word

---
+++ 058_Length of Last Word.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <sstream>
+
+class Solution
+{
+
+public:
+
+	int lengthOfLastWord(std::string s)
+	{
+		std::stringstream stream = std::stringstream(s);
+		std::string word;
+		int lastLength = 0;
+		while(stream >> word)
+		{
+			lastLength = word.length();
+		}
+
+		return lastLength;
+	}
+};
