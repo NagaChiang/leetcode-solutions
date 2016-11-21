56461f5d2833ef838111111079aef3e26e0e8751
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-21 10:59:32 +0800

    225. Implement Stack using Queues

---
+++ 225_Implement Stack using Queues.cpp
@@ -0,0 +1,38 @@
+﻿#include <queue>
+
+class Stack
+{
+	std::queue<int> _queue;
+
+public:
+
+	// Push element x onto stack.
+	void push(int x)
+	{
+		// Push and reorder size() - 1
+		_queue.push(x);
+		for(int i = 0; i < _queue.size() - 1; i++)
+		{
+			_queue.push(_queue.front());
+			_queue.pop();
+		}
+	}
+
+	// Removes the element on top of the stack.
+	void pop()
+	{
+		_queue.pop();
+	}
+
+	// Get the top element.
+	int top()
+	{
+		return _queue.front();
+	}
+
+	// Return whether the stack is empty.
+	bool empty()
+	{
+		return _queue.empty();
+	}
+};
