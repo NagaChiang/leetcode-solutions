df960161ce2dbdb7366f08d83f0e7c0824ef5ed1
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-22 14:31:09 +0800

    190. Reverse Bits

---
+++ 190_Reverse Bits.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdint.h>
+
+class Solution
+{
+
+public:
+
+	uint32_t reverseBits(uint32_t n)
+	{
+		int reversed[16] = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+		int result = 0;
+		int mask = 0xF;
+		for(int i = 0; i < 8; i++)
+		{
+			result <<= 4;
+			result |= reversed[mask & n];
+			n >>= 4;
+		}
+
+		return result;
+	}
+};
