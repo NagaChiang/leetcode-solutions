039baff91ae65be9c974336e419678cb52edd58f
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-23 15:14:05 +0800

    204. Count Primes

---
+++ 204_Count Primes.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+
+// Sieve of Eratosthenes
+class Solution
+{
+
+public:
+
+	int countPrimes(int n)
+	{
+		// Mark all the non-primes
+		std::vector<bool> isPrime = std::vector<bool>(n, true)
+		for(int i = 2; i * i < n; i++)
+		{
+			if(!isPrime[i])
+				continue;
+
+			for(int j = i * i; j < n; j += i)
+				isPrime[j] = false;
+		}
+
+		// Count primes
+		int count = 0;
+		for(int i = 2; i < n; i++)
+		{
+			if(isPrime[i])
+				count++;
+		}
+
+		return count;
+	}
+};
