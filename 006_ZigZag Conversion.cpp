7daa314938dc7d0c709ca22eea543db799010655
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-23 15:56:50 +0800

    6. ZigZag Conversion

---
+++ 006_ZigZag Conversion.cpp
@@ -0,0 +1,40 @@
+﻿#include <string>
+#include <vector>
+
+class Solution
+{
+
+public:
+
+	std::string convert(std::string s, int numRows)
+	{
+		// Generate zigzag string matrix
+		std::vector<std::string> matrix(numRows, std::string());
+		int row = 0;
+		bool isDown = true;
+		for(int i = 0; i < s.length(); i++)
+		{
+			// Append char
+			matrix[row].push_back(s[i]);
+
+			if(numRows > 1)
+			{
+				// Next row
+				row += isDown ? 1 : -1;
+
+				// Change direction
+				if(row == 0 || row == numRows - 1)
+					isDown = !isDown;
+			}
+		}
+
+		// Concatenate strings
+		std::string zigzag;
+		for(int i = 0; i < matrix.size(); i++)
+		{
+			zigzag.append(matrix[i]);
+		}
+
+		return zigzag;
+	}
+};
