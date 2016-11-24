505584dd3a90f71df0458ba6f0dd1538829d30d3
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-24 20:33:15 +0800

    278. First Bad Version

---
+++ 278_First Bad Version.cpp
@@ -0,0 +1,33 @@
+﻿// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+// Binary search
+class Solution
+{
+
+public:
+
+	int firstBadVersion(int n)
+	{
+		int left = 0;
+		int right = n;
+		while(left < right)
+		{
+			int mid = left + (right - left) / 2;
+
+			// Test mid
+			if(isBadVersion(mid))
+			{
+				// Bad: in the left half
+				right = mid;
+			}
+			else
+			{
+				// Good: in the right half
+				left = mid + 1;
+			}
+		}
+
+		return left;
+	}
+};
