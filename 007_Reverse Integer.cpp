6b26ca8617ae11f122c9373578c685119ba152d3
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-25 18:22:28 +0800

    7. Reverse Integer

---
+++ 007_Reverse Integer.cpp
@@ -0,0 +1,26 @@
+﻿class Solution
+{
+
+public:
+
+	int reverse(int x)
+	{
+		int result = 0;
+		while(x != 0)
+		{
+			int nextResult = result * 10 + x % 10;
+			x /= 10;
+
+			// Test overflow
+			if(nextResult / 10 != result)
+			{
+				result = 0;
+				break;
+			}
+
+			result = nextResult;
+		}
+
+		return result;
+	}
+};
