d89d416b929d4fdfd75e583b603924aeebba1310
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-11-26 15:55:37 +0800

    419. Battleships in a Board

---
+++ 419_Battleships in a Board.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+class Solution
+{
+
+public:
+
+	int countBattleships(std::vector<std::vector<char>>& board)
+	{
+		int count = 0;
+		for(int i = 0; i < board.size(); i++)
+		{
+			for(int j = 0; j < board[0].size(); j++)
+			{
+				if(board[i][j] == 'X'
+					&& (i == 0 || board[i - 1][j] != 'X')
+					&& (j == 0 || board[i][j - 1] != 'X'))
+				{
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+};
