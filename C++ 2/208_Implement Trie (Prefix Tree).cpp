ad3aee99d39058fe57478fc30fccd75476f46758
Naga Chiang <nagachiangdev@gmail.com>
2021-07-27 11:38:59 +0800

    208_Implement Trie (Prefix Tree)

---
+++ C++ 2/208_Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,57 @@
+class Trie {
+private:
+    struct TrieNode {
+        TrieNode* children[26];
+        bool isEnd;
+        
+        TrieNode() {
+            memset(children, 0, sizeof(children));
+            isEnd = false;
+        }
+    };
+    
+    typedef struct TrieNode TrieNode;
+    
+    TrieNode* root;
+    
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (const char c : word) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                node->children[index] = new TrieNode();
+            }
+            
+            node = node->children[index];
+        }
+        
+        node->isEnd = true;
+    }
+
+    bool search(const string& word) {
+        TrieNode* node = findPrefixEnd(word);
+        return node != nullptr && node->isEnd;
+    }
+
+    bool startsWith(const string& prefix) {
+        return findPrefixEnd(prefix) != nullptr;
+    }
+
+private:
+    TrieNode* findPrefixEnd(const string& prefix) {
+        TrieNode* node = root;
+        for (const char c : prefix) {
+            node = node->children[c - 'a'];
+            if (node == nullptr) {
+                return nullptr;
+            }
+        }
+        
+        return node;
+    }
+};
