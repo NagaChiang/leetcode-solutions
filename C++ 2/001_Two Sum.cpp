186d5094513a6693b9c2502784827d4186aa486d
Naga Chiang <nagachiangdev@gmail.com>
2021-08-19 10:49:32 +0800

    Update 001_Two Sum

--- C++ 2/001_Two Sum.cpp
+++ C++ 2/001_Two Sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> complementToIndexMap;
+        vector<int> indexes;
+        unordered_map<int, int> complementMap;
         for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            auto itr = complementToIndexMap.find(num);
-            if (itr != complementToIndexMap.end()) {
-                return {itr->second, i};
+            if (complementMap.find(nums[i]) != complementMap.end()) {
+                indexes.push_back(i);
+                indexes.push_back(complementMap[nums[i]]);
+                break;
             }
             
-            complementToIndexMap[target - num] = i;
+            complementMap[target - nums[i]] = i;
         }
         
-        return {};
+        return indexes;
     }
 };
