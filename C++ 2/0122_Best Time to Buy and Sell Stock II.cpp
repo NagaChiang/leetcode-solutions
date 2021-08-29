d72dcc14eb55798273691ac294cca150e4d2e467
Naga Chiang <nagachiangdev@gmail.com>
2021-08-29 16:14:26 +0800

    Update 0122_Best Time to Buy and Sell Stock II

--- C++ 2/0122_Best Time to Buy and Sell Stock II.cpp
+++ C++ 2/0122_Best Time to Buy and Sell Stock II.cpp
@@ -35,3 +35,21 @@ public:
         return profit;
     }
 };
+
+// DP
+// At the end of the day:
+// 0: Have stock
+// 1: No stock
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> profits(prices.size(), vector<int>(2));
+        profits[0][0] = -prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            profits[i][0] = max(profits[i - 1][0], profits[i - 1][1] - prices[i]); // Bought before or buy today
+            profits[i][1] = max(profits[i - 1][1], profits[i - 1][0] + prices[i]); // No stock before or sell today
+        }
+        
+        return profits.back()[1];
+    }
+};
