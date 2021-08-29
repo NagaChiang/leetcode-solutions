375410d8aa77d44115c32d314e01d4e48cfcd233
Naga Chiang <nagachiangdev@gmail.com>
2021-08-29 10:59:19 +0800

    Update 0053_Maximum Subarray

--- C++ 2/0053_Maximum Subarray.cpp
+++ C++ 2/0053_Maximum Subarray.cpp
@@ -11,3 +11,22 @@ public:
         return globalMax;
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        vector<int> maxSumUntil(nums.size());
+        maxSumUntil[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            maxSumUntil[i] = nums[i];
+            if (maxSumUntil[i - 1] > 0) {
+                maxSumUntil[i] += maxSumUntil[i - 1];
+            }
+            
+            maxSum = max(maxSum, maxSumUntil[i]);
+        }
+        
+        return maxSum;
+    }
+};
