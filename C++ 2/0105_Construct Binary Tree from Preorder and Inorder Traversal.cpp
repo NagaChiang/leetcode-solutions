86ca140bd3da6ac6a2686c2eb6025dc80e155b14
Naga Chiang <nagachiangdev@gmail.com>
2021-09-08 14:53:58 +0800

    Update 0105_Construct Binary Tree from Preorder and Inorder Traversal

--- C++ 2/0105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ C++ 2/0105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -5,24 +5,21 @@ public:
         for (int i = 0; i < inorder.size(); i++) {
             inorderIndexMap[inorder[i]] = i;
         }
-        
+
         return buildTree(preorder, inorder, inorderIndexMap, 0, 0, preorder.size());
     }
-    
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& inorderIndexMap, int preStart, int inStart, int length) {
-        if (length <= 0) {
+
+private:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& inorderIndexMap, int preStart, int inStart, int treeSize) {
+        if (treeSize == 0) {
             return nullptr;
         }
-        
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        if (length == 1) {
-            return root;
-        }
-        
-        int inRootIndex = inorderIndexMap[preorder[preStart]] - inStart;
-        root->left = buildTree(preorder, inorder, inorderIndexMap, preStart + 1, inStart, inRootIndex);
-        root->right = buildTree(preorder, inorder, inorderIndexMap, preStart + inRootIndex + 1, inStart + inRootIndex + 1, length - inRootIndex - 1);
-            
-        return root;
+
+        TreeNode* node = new TreeNode(preorder[preStart]);
+        int inRootOffset = inorderIndexMap[preorder[preStart]] - inStart;
+        node->left = buildTree(preorder, inorder, inorderIndexMap, preStart + 1, inStart, inRootOffset);
+        node->right = buildTree(preorder, inorder, inorderIndexMap, preStart + inRootOffset + 1, inStart + inRootOffset + 1, treeSize - inRootOffset - 1);
+
+        return node;
     }
 };
