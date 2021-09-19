78566add92d1bc2c3656e3871cda4fedb7e492c5
Naga Chiang <nagachiangdev@gmail.com>
2021-09-19 10:22:05 +0800

    Update 0238_Product of Array Except Self

--- C++ 2/0238_Product of Array Except Self.cpp
+++ C++ 2/0238_Product of Array Except Self.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> products(nums.size(), 1);
+
+    int left = 1;
+    int right = 1;
+    for (int i = 0; i < nums.size(); i++) {
+      products[i] *= left;
+      left *= nums[i];
+
+      int revIndex = nums.size() - 1 - i;
+      products[revIndex] *= right;
+      right *= nums[revIndex];
+    }
+
+    return products;
+  }
+};
+
+class Solution {
+public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> answers;
 
