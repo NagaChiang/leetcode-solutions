694c4f732d672429dd3c42b1224bd3f0846f3e35
Naga Chiang <nagachiangdev@gmail.com>
2021-10-21 11:01:14 +0800

    Update 0057_Insert Interval

--- C++ 2/0057_Insert Interval.cpp
+++ C++ 2/0057_Insert Interval.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        bool inserted = false;
+        vector<vector<int>> results;
+        for (const auto& interval : intervals) {
+            if (inserted) {
+                results.push_back(interval);
+            } else {
+                if (interval[1] < newInterval[0]) {
+                    // Before new interval
+                    results.push_back(interval);
+                } else if (newInterval[1] < interval[0]) {
+                    // After new interval
+                    results.push_back(newInterval);
+                    results.push_back(interval);
+                    inserted = true;
+                } else {
+                    // Overlapped
+                    newInterval[0] = min(interval[0], newInterval[0]);
+                    newInterval[1] = max(interval[1], newInterval[1]);
+                }
+            }
+        }
+
+        if (!inserted) {
+            results.push_back(newInterval);
+        }
+
+        return results;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> newIntervals; 
         bool inserted = false;
         for (const auto& interval : intervals) {
