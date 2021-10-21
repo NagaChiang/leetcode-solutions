5022240f573b8484559074dee09ba9ff3a9d8788
Naga Chiang <nagachiangdev@gmail.com>
2021-10-21 11:29:06 +0800

    Update 0056_Merge Intervals

--- C++ 2/0056_Merge Intervals.cpp
+++ C++ 2/0056_Merge Intervals.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] < b[0];
-        });
-        
-        vector<vector<int>> results;
-        results.push_back(intervals[0]);
-        for (auto& interval : intervals) {
-            vector<int>& lastInterval = results.back();
-            if (lastInterval[1] >= interval[0]) {
-                lastInterval = {lastInterval[0], max(lastInterval[1], interval[1])};
-            } else {
-                results.push_back(interval);
-            }
-        }
-        
-        return results;
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> results;
+    results.push_back(intervals[0]);
+    for (const auto& interval : intervals) {
+      auto& last = results.back();
+      if (last[1] >= interval[0]) {
+        last[1] = max(last[1], interval[1]);
+      } else {
+        results.push_back(interval);
+      }
     }
+
+    return results;
+  }
 };
