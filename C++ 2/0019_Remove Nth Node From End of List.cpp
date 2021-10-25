c70783dcfd3aecfb8229756bf0a66328687f0418
Naga Chiang <nagachiangdev@gmail.com>
2021-10-25 10:47:38 +0800

    Update 0019_Remove Nth Node From End of List

--- C++ 2/0019_Remove Nth Node From End of List.cpp
+++ C++ 2/0019_Remove Nth Node From End of List.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* newHead = new ListNode(0, head);
-        ListNode* slow = newHead;
-        ListNode* fast = head;
-        for (int i = 0; i < n; i++) {
-            fast = fast->next;
-        }
-        
-        
-        while (fast != nullptr) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-        
-        ListNode* removeNode = slow->next;
-        slow->next = slow->next->next;
-        delete removeNode;
-        
-        head = newHead->next;
-        delete newHead;
-        
-        return head;
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode preHead = ListNode(0, head);
+    ListNode* slow = &preHead;
+    ListNode* fast = &preHead;
+    for (int i = 0; i < n; i++) {
+      fast = fast->next;
     }
+
+    while (fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next;
+    }
+
+    ListNode* temp = slow->next;
+    slow->next = slow->next->next;
+    delete temp;
+
+    return preHead.next;
+  }
 };
 
 class Solution {
